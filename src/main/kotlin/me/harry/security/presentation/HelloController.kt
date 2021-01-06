4392582b071842a29fff829fb82b7b3625bd3493
minkukjo <strike0115@naver.com>
2021-01-06 23:40:33 +0900

    Spring Security 기초 실습

---
+++ src/main/kotlin/me/harry/security/presentation/HelloController.kt
@@ -0,0 +1,19 @@
+package me.harry.security.presentation
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun sayHello(name: String): String {
+        return "Hello $name"
+    }
+
+    @GetMapping("/admin/hello")
+    fun sayAdminHello(name: String): String {
+        return "Hello Admin $name"
+    }
+
+}
